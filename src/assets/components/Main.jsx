3ed1e161e826772e29d44b0e6b855dfdb3fcd43f
nainsworth <nja8161995@gmail.com>
2025-11-11 14:04:23 -0600

    Refactor Array State

--- src/assets/components/Main.jsx
+++ src/assets/components/Main.jsx
@@ -1,10 +1,32 @@
+import { useState } from "react";
+
+
 const Main = () => {
+  const [ingredients, setIngredients] = useState([]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const newIngredient = formData.get("ingredient");
+    setIngredients(prevIngredients => [...prevIngredients, newIngredient]);
+  };
+
   return (
     <main>
-      <form className="ingredient-form">
-        <input type="text" placeholder="e.g. oregano" aria-label="Add ingredient" />
+      <form className="ingredient-form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="e.g. oregano"
+          aria-label="Add ingredient"
+          name="ingredient"
+        />
         <button>+ Add Ingredient</button>
       </form>
+      <ul>
+        {ingredients.map((ingredient, index) => (
+          <li key={index}>{ingredient}</li>
+        ))}
+      </ul>
     </main>
   );
 };
